d8f9461d42e74abe9b51438b022df3812e907bb7
Alexandra Shparberg <alexandras@alexandras-air.mynetworksettings.com>
2024-11-29 16:49:15 -0500

    modified

---
+++ reactjs-project/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* Your Favorite Welcome Message */}
+        <p>Welcome to our corner of the internet! 🌐</p>
+        <p>
+          We're here to inspire, inform, and connect. Take your time to explore, and let us know 
+          how we can make your experience even better. Happy browsing!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
